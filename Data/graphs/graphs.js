bd647e28e76e8bdc4f781a24b5a6aa5eda0152f7
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2022-12-15 01:17:16 -0500

    graphs

---
+++ Data/graphs/graphs.js
@@ -0,0 +1,18 @@
+// edge list
+const edgeListGraph = [
+    [0, 2],
+    [2, 3],
+    [2, 1],
+    [1, 3],
+];
+
+// adjacent list
+const adjListGraph = [[2], [2, 3], [0, 1, 3], [1, 2]];
+
+// adjacent matrix
+const matrix = [
+    [1, 2, 3],
+    [1, -2, 3],
+    [2, -1, 1],
+];
+
