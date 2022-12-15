4a0011bf9bde7f247a7be87bfee6432139d8b418
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2022-12-15 13:28:35 -0500

    bubble sort

---
+++ Algos/sorting/bubble-sort.js
@@ -0,0 +1,13 @@
+const bubbleSort = array => {
+    let arrayHasChanged;
+    while (true) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i + 1] && array[i] > array[i + 1]) {
+                arrayHasChanged = true;
+                [array[i], array[i + 1]] = [array[i + 1], array[i]];
+            }
+        }
+        if (!arrayHasChanged) return array;
+        arrayHasChanged = false;
+    }
+};
