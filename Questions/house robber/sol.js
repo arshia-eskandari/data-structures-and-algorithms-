3d0391bef84edce4cb694c8dfa8e50f3659cc4c2
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2022-12-26 20:43:23 -0500

    more questions

--- Questions/house robber/sol.js
+++ Questions/house robber/sol.js
@@ -4,30 +4,14 @@
  */
 
 // [1, 2, 4, 5, 6, 7, 3]
-var rob = function (nums) {
-    if (nums.length === 0) return 0;
-    if (nums.length === 1) return nums[0];
-    if (nums.length === 2) {
-        if (nums[0] > nums[1]) return nums[0];
-        else return nums[1];
+let rob = function(nums) {
+    let previous = 0;
+    let current = 0;
+    let temp;
+    for (let x of nums) {
+        temp = current;
+        current = Math.max(x+previous, current);
+        previous = temp;
     }
-
-    let startingIndex = 0; // 1
-    let secondStartingPoint = startingIndex + 2; // 3 -> 4
-    let index = secondStartingPoint;
-    let max = 0;
-
-    while (startingIndex + secondStartingPoint <= 5) {
-        let currentMax = nums[startingIndex];
-        while (index < nums.length) {
-            currentMax += nums[index];
-            index += 2;
-        }
-        max = Math.max(max, currentMax);
-        if (startingIndex === 0) {
-            if (secondStartingPoint === 2) startingIndex = 1;
-            else secondStartingPoint = 2;
-        }
-    }
-    return max;
-};
+    return current;
+}
