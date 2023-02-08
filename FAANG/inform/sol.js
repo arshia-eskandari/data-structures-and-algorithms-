01093e525de3733332fba85c82672052a9fb2abb
arshia-eskandari <arshia.eskandari.3000@gmail.com>
2023-02-08 02:25:56 -0500

    inform q

---
+++ FAANG/inform/sol.js
@@ -0,0 +1,23 @@
+const dFS = (id, adjList, informTime) => {
+    if (!adjList[id].length) return 0;
+
+    let max = 0;
+    for (const subID of adjList[id]) {
+        max = Math.max(max, dFS(subID, adjList, informTime));
+    }
+
+    return max + informTime[id];
+};
+
+var numOfMinutes = function (n, headID, manager, informTime) {
+    const adjList = managers.map(() => []);
+
+    for (let employee = 0; employee < n; employee++) {
+        const manager = managers[employee];
+        if (manager === -1) continue;
+
+        adjList[manager].push(employee);
+    }
+
+    return dFS(headID, adjList, informTime);
+};
